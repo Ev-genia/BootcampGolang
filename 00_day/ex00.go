ec610847218aeabb21ebc89b9314378d5afc70dc
Ev-genia <bzhenya2006@ya.ru>
2023-05-26 12:05:44 +0300

    add standard deviation to 00day

--- 00_day/ex00.go
+++ 00_day/ex00.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -36,6 +37,8 @@ func main() {
 	// fmt.Println("str[len(str)]: ", str[len(str)-2:len(str)-1])
 	// fmt.Println("TypeOf(str[len(str)]): ", reflect.TypeOf(str[len(str)-2:len(str)-1]))
 	nbrsMap := make(map[int]int)
+	var nbrsReadArr []int
+	// j := 0
 	if str[len(str)-2:len(str)-1] >= "0" && str[len(str)-2:len(str)-1] <= "9" {
 		num, err := strconv.Atoi(str[len(str)-2 : len(str)-1])
 		if err == nil {
@@ -44,6 +47,7 @@ func main() {
 			nbrsMap[num] = 1
 			countNbr++
 			sumNbr += num
+			nbrsReadArr = append(nbrsReadArr, num)
 		}
 	}
 	// str += "\\n"              //
@@ -61,6 +65,7 @@ func main() {
 			}
 			countNbr++
 			sumNbr += num
+			nbrsReadArr = append(nbrsReadArr, num)
 		}
 	}
 
@@ -73,7 +78,7 @@ func main() {
 
 	mean := 0.0
 	mean = float64(sumNbr) / float64(countNbr)
-	fmt.Println("Mean: ", mean)
+	fmt.Printf("Mean: %.2f\n", mean)
 
 	// fmt.Println("len map: ", len(nbrsMap)) //
 
@@ -87,31 +92,49 @@ func main() {
 		nbrsArr = append(nbrsArr, sPair{key, val})
 	}
 
-	// fmt.Println("Arr:")            //
-	// for _, elem := range nbrsArr { //
-	// 	fmt.Println(elem, " ") //
-	// } //
+	// // fmt.Println("Arr:")            //
+	// // for _, elem := range nbrsArr { //
+	// // 	fmt.Println(elem, " ") //
+	// // } //
 
 	sort.Slice(nbrsArr, func(i, j int) bool {
 		return nbrsArr[i].val > nbrsArr[j].val
 	})
 
-	// fmt.Println("Arr sort:")       //
-	// for _, elem := range nbrsArr { //
-	// 	fmt.Println(elem, " ") //
-	// } //
+	// // fmt.Println("Arr sort:")       //
+	// // for _, elem := range nbrsArr { //
+	// // 	fmt.Println(elem, " ") //
+	// // } //
 
 	mode := nbrsArr[0]
 	fmt.Println("Mode: ", mode.key)
 
-	// fmt.Println("len arr: ", len(nbrsArr)) //
-	// fmt.Println("len/2 arr: ", len(nbrsArr)/2) //
+	// // fmt.Println("len arr: ", len(nbrsArr)) //
+	// // fmt.Println("len/2 arr: ", len(nbrsArr)/2) //
 	var median float64
-	if len(nbrsArr)%2 == 0 {
-		median = (float64(nbrsArr[len(nbrsArr)/2-1].key) + float64(nbrsArr[len(nbrsArr)/2].key)) / 2
-	} else {
-		median = float64(nbrsArr[len(nbrsArr)/2].key)
-	}
-	fmt.Println("Median: ", median)
+	// if len(nbrsArr)%2 == 0 {
+	// 	median = (float64(nbrsArr[len(nbrsArr)/2-1].key) + float64(nbrsArr[len(nbrsArr)/2].key)) / 2
+	// } else {
+	// 	median = float64(nbrsArr[len(nbrsArr)/2].key)
+	// }
+	// fmt.Println("Median map: ", median)
+	// fmt.Println("read arr: ", nbrsReadArr)
 
+	sort.Slice(nbrsReadArr, func(i, j int) bool {
+		return nbrsReadArr[i] < nbrsReadArr[j]
+	})
+	// fmt.Println("read arr sort: ", nbrsReadArr)
+	if len(nbrsReadArr)%2 == 0 {
+		median = (float64(nbrsReadArr[len(nbrsReadArr)/2-1]) + float64(nbrsReadArr[len(nbrsReadArr)/2])) / 2
+	} else {
+		median = float64(nbrsReadArr[len(nbrsReadArr)/2])
+	}
+	fmt.Printf("Median: %.2f\n", median)
+	var sDivitation float64
+	for _, val := range nbrsReadArr {
+		sDivitation += math.Pow(float64(val)-mean, 2)
+	}
+	sDivitation = sDivitation / float64(countNbr-1)
+	sDivitation = math.Sqrt(sDivitation)
+	fmt.Printf("SD: %.2f\n", sDivitation)
 }

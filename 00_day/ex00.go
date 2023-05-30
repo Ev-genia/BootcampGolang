54ae3f53582bb890d6de37701e915030dc33fa5d
Ev-genia <bzhenya2006@ya.ru>
2023-05-30 16:51:41 +0300

    del comments

--- 00_day/ex00.go
+++ 00_day/ex00.go
@@ -7,13 +7,9 @@ import (
 	"io"
 	"math"
 	"os"
-	"strings"
-
-	// "os"
 	"sort"
 	"strconv"
-	// "strings"
-	// "flag"
+	"strings"
 )
 
 type sMethod struct {
@@ -35,93 +31,6 @@ type sPair struct {
 }
 
 type PairList []sPair
-
-// type Interface interface {
-// 	Len() int
-// }
-
-// func scan() string {
-// 	in := bufio.NewReader(os.Stdin)
-// 	str, err := in.ReadString('\n')
-// 	fmt.Println("str in scan: ", str) //
-// 	if err != nil {
-// 		fmt.Println("Error of enter", err)
-// 	}
-// 	return str
-// }
-
-/*
-func readData(str string) (int, int, map[int]int, []int) {
-	countNbr := 0
-	sumNbr := 0
-	nbrsMap := make(map[int]int)
-	var nbrsReadArr []int
-	// if str[len(str)-2:len(str)-1] >= "0" && str[len(str)-2:len(str)-1] <= "9" {
-	// 	num, err := strconv.Atoi(str[len(str)-2 : len(str)-1])
-	// 	if err == nil && num >= -100000 && num <= 100000 {
-	// 		nbrsMap[num] = 1
-	// 		countNbr++
-	// 		sumNbr += num
-	// 		nbrsReadArr = append(nbrsReadArr, num)
-	// 	} else {
-	// 		fmt.Println("wrong agrument")
-	// 		os.Exit(1)
-	// 	}
-	// }
-	words := strings.Split(str, "\\n")
-	for _, i := range words[:len(words)-1] {
-		num, err := strconv.Atoi(i)
-		fmt.Println("map: ", nbrsMap) //
-		fmt.Println("err: ", err)     //
-
-		if err == nil { //&& (num >= -100000 && num <= 100000) {
-			check, _ := nbrsMap[num]
-			if check == 0 { //&& ok == false {
-				nbrsMap[num] = 1
-			} else {
-				nbrsMap[num] += 1
-			}
-			countNbr++
-			sumNbr += num
-			nbrsReadArr = append(nbrsReadArr, num)
-		} else {
-			fmt.Println("wrong agrument")
-			os.Exit(1)
-		}
-	}
-	lastWordArr := words[len(words)-1:]
-	lastWord := lastWordArr[0]
-	strings.TrimSpace(lastWord)
-	fmt.Print("lastWord: ", lastWord)
-	fmt.Println("map at the end: ", nbrsMap) //
-	number, err := strconv.Atoi(lastWord)
-	fmt.Println("err Atoi number: ", err)
-	fmt.Println("number: ", number)
-
-	return countNbr, sumNbr, nbrsMap, nbrsReadArr
-}
-*/
-
-/*
-func readData(str string) (int, int, map[int]int, []int) {
-	countNbr := 0
-	sumNbr := 0
-
-	fmt.Print("str: ", str)
-	var nbrsReadArr []int
-	strArr := strings.Split(str, "\\n")
-	fmt.Println("strArr: ", strArr)
-	for _, s := range strArr {
-		i, _ := strconv.Atoi(s)
-		nbrsReadArr = append(nbrsReadArr, i)
-	}
-	fmt.Println("nbrsReadArr: ", nbrsReadArr)
-
-	nbrsMap := make(map[int]int)
-
-	return countNbr, sumNbr, nbrsMap, nbrsReadArr
-}
-*/
 
 func scaningNbr() (bool, float64, bool) {
 	endRead := false
@@ -176,7 +85,6 @@ func scaningNbr() (bool, float64, bool) {
 func makeMean(sumNbr float64, countNbr int) float64 {
 	mean := 0.0
 	mean = float64(sumNbr) / float64(countNbr)
-	// fmt.Printf("Mean: %.2f\n", mean)
 	return mean
 }
 
@@ -239,8 +147,6 @@ func handler(nbrsReadArr []float64, nbrsMap map[int]int, countNbr int, sumNbr fl
 }
 
 func main() {
-	// str := scan()
-
 	method := sMethod{false, false, false, false, true}
 	var printUsage bool
 	flag.BoolVar(&method.meanMethod, "mean", false, "show mean of arguments")
@@ -266,7 +172,7 @@ func main() {
 			sumNbr += valFloat
 			countNbr++
 			check, _ := nbrsMap[int(valFloat)]
-			if check == 0 { //&& ok == false {
+			if check == 0 {
 				nbrsMap[int(valFloat)] = 1
 			} else {
 				nbrsMap[int(valFloat)] += 1
@@ -282,48 +188,4 @@ func main() {
 	} else {
 		fmt.Println("Values no entered")
 	}
-
-	// // countNbr, sumNbr, nbrsMap, nbrsReadArr := readData(str)
-	// //add check -100000 и 100000
-	// fmt.Println("\nPlease, choose one of the available commands:")
-	// fmt.Println("1.\tShow mean\n2.\tShow median\n3.\tShow mode")
-	// fmt.Println("4.\tShow standart divitation\n5.\tShow all metrics\n6.\tExit program")
-	// fmt.Println("Please, choose one and enter command number:")
-	// fmt.Println()
-
-	// var method int
-	// for {
-	// 	fmt.Scan(&method)
-	// 	if method >= 1 && method <= 6 {
-	// 		break
-	// 	} else {
-	// 		fmt.Println("Enter digit from 1 to 6")
-	// 	}
-	// }
-
-	// mean := makeMean(float64(sumNbr), float64(countNbr))
-
-	// switch method {
-	// case 1:
-	// 	fmt.Printf("Mean: %.2f\n", mean)
-	// case 2:
-	// 	makeMedian(nbrsReadArr)
-	// case 3:
-	// 	makeMode(nbrsMap)
-	// case 4:
-	// 	makeStandartDivitation(nbrsReadArr, mean, countNbr)
-	// case 5:
-	// 	fmt.Printf("Mean: %.2f\n", mean)
-	// 	makeMedian(nbrsReadArr)
-	// 	makeMode(nbrsMap)
-	// 	makeStandartDivitation(nbrsReadArr, mean, countNbr)
-	// case 6:
-	// 	return
-	// default:
-	// 	fmt.Println("Unknown symbol")
-	// }
-
-	// // makeMode(nbrsMap)
-	// // makeMedian(nbrsReadArr)
-	// // makeStandartDivitation(nbrsReadArr, mean, countNbr)
 }

be40060282bdd1f18dceb6ab3fae67da6b886342
Ev-genia <bzhenya2006@ya.ru>
2023-05-31 11:14:58 +0300

    dix ex00 day02

--- 02_day/01_00/ex00.go
+++ 02_day/01_00/ex00.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type sFlag struct {
@@ -21,18 +22,22 @@ func showAllCommands() {
 	os.Exit(0)
 }
 
-func recursiveFind(pathArg string, typeFind string) {
+func recursiveFind(pathArg string, typeFind string, extArg string) {
 	filepath.WalkDir(pathArg, func(path string, file fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
+		fi, err := file.Info()
 		if file.IsDir() && (typeFind == "d" || typeFind == "") {
 			fmt.Println(path)
 		}
-		if !file.IsDir() && (typeFind == "f" || typeFind == "") {
-			fmt.Println(path)
+		if !file.IsDir() && ((typeFind == "f" || typeFind == "") || (typeFind == "d" && extArg != "")) {
+			if strings.EqualFold(extArg, "") {
+				fmt.Println(path)
+			} else if strings.EqualFold(filepath.Ext(path), extArg) && extArg != "" {
+				fmt.Println(path)
+			}
 		}
-		fi, err := file.Info()
 		if (fi.Mode()&fs.ModeSymlink != 0) && typeFind == "sl" {
 			eval, err := filepath.EvalSymlinks(fi.Name())
 			if err != nil && os.IsNotExist(err) {
@@ -55,26 +60,39 @@ func main() {
 	flag.BoolVar(&flags.symbolicLink, "sl", false, "find symbolic link")
 	flag.BoolVar(&flags.dir, "d", false, "find directory")
 	flag.BoolVar(&flags.file, "f", false, "find file")
-	flag.StringVar(&extArg, "ext", "", "find only files with a certain extension")
+	flag.StringVar(&extArg, "ext", "", "find only files with a certain extension, work only with -f")
 	flag.BoolVar(&printUsage, "h", false, "show all commands")
 	flag.Parse()
 	if printUsage {
 		showAllCommands()
 	}
 	args := os.Args[1:]
-	pathArg = args[len(args)-1]
+	if len(args) > 0 {
+		pathArg = args[len(args)-1]
+	} else {
+		pathArg = "."
+	}
+	if _, err := os.Stat(pathArg); os.IsNotExist(err) {
+		fmt.Printf("%s is not exist\n", pathArg)
+	}
 	if len(args) == 1 {
-		recursiveFind(pathArg, "")
+		recursiveFind(pathArg, "", extArg)
 	} else {
 		flags.allFlags = !(flags.symbolicLink || flags.dir || flags.file)
 		if flags.allFlags || flags.dir {
-			recursiveFind(pathArg, "d")
+			recursiveFind(pathArg, "d", extArg)
 		}
-		if flags.allFlags || flags.file {
-			recursiveFind(pathArg, "f")
+		if !flags.file {
+			extArg = ""
+		}
+		if !(strings.EqualFold(extArg, "")) {
+			extArg = "." + extArg
+		}
+		if flags.allFlags || flags.file || (extArg != "" && flags.file) {
+			recursiveFind(pathArg, "f", extArg)
 		}
 		if flags.allFlags || flags.symbolicLink {
-			recursiveFind(pathArg, "sl")
+			recursiveFind(pathArg, "sl", extArg)
 		}
 	}
 }
